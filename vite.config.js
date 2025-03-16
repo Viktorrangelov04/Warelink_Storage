951726491b1c2f4a771eba193391064909364fc7
Viktor Rangelov <viktorrangelov0@gmail.com>
2025-03-17 01:22:30 +0200

    fixing file structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindConfig from './config/tailwind.config.js'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './config/postcss.config.js'
+  }
 })
